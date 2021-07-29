b85b6d01889ad823ed9ac1df255cedefc444ffaa
Youssef Bayouli <bayouliyoussef@gmail.com>
2021-07-29 11:30:14 +0100

    adding tag to docker image

--- blue/run_docker.sh
+++ blue/run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=test_blue_image .
+docker build --tag=test_blue_image:latest .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -p 8000:80 test_blue_image
+docker run -p 8000:80 test_blue_image:latest
